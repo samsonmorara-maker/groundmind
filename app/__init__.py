40c6273d158cc18cf5e72a49c1de26aaa5289347
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-26 20:45:19 +0300

    addititon of posts

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-
-
-
+Bootstrap(app)
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
 from flask import render_template
 
-
+posts=[
+    {"id":1 , "title":"Can AI master the Perfect Lawn?","excerpt":"Universal Autonomous Robotic Lawn Mowe", "content":"A next-generation, wire-free, GPS-free lawn-care system engineered to adapt seamlessly to any outdoor environment"},
+    {"id":2 , "title":"Can AI master the Perfect Lawn?","excerpt":"Universal Autonomous Robotic Lawn Mowe", "content":"A next-generation, wire-free, GPS-free lawn-care system engineered to adapt seamlessly to any outdoor environment"},
+    {"id":3 , "title":"Can AI master the Perfect Lawn?","excerpt":"Universal Autonomous Robotic Lawn Mowe", "content":"A next-generation, wire-free, GPS-free lawn-care system engineered to adapt seamlessly to any outdoor environment"}
+]
 
 @app.route("/")
 def home ():
